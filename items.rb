5f604565b6b505056c0346259b72a6859a367151
faisalakb <faisalakb786@gmail.com>
2023-10-31 13:42:16 +0500

    add items information

---
+++ items.rb
@@ -0,0 +1,42 @@
+class Item
+    attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
+  
+    def initialize(id, genre, author, source, label, publish_date, archived)
+      @id = id
+      @genre = genre
+      @author = author
+      @source = source
+      @label = label
+      @publish_date = publish_date
+      @archived = archived
+    end
+  
+    def can_be_archived?
+      age_in_years = (Date.today - @publish_date).to_i / 365
+      age_in_years > 10
+    end
+  
+    def move_to_archive
+      if can_be_archived?
+        @archived = true
+      end
+    end
+  
+    # Custom setter methods for 1-to-many relationships
+    def set_genre(genre)
+      @genre = genre
+    end
+  
+    def set_author(author)
+      @author = author
+    end
+  
+    def set_source(source)
+      @source = source
+    end
+  
+    def set_label(label)
+      @label = label
+    end
+  end
+  
